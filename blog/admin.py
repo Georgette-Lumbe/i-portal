f8818aba0742f6cb4ce149f04a79e3e47e7c4222
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-10-25 21:56:55 +0000

    Register post and comment models into admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+# Register models into admin panel
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Make blog writing easy
+    """
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title']
+    list_filter = ('status', 'created_on')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Manage Comments
+    """
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
